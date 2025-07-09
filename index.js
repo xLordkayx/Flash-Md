d2f4ee7b7a873bec3e93c93f9ee44144ac80548c
France King <151246926+franceking1@users.noreply.github.com>
2025-07-09 16:00:05 +0300

    JULY UPDATE 👍

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('FLASH-MD V2.5  is running!'));
-app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`FLASH-MD Server running on port ${PORT}`));
 
 require('./db-init')();
 require('./king');
